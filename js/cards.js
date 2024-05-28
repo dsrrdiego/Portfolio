52a2f6dea8b67e798e248bc287ff25c05d2f816d
Diego Sebastian Rodriguez Recalde <dsrrdiego@hotmail.com>
2024-05-27 21:04:03 -0300

    carrusel va

--- js/cards.js
+++ js/cards.js
@@ -26,7 +26,7 @@ let cards = [{
         "categoria": "Spring Boot Api",
         "fecha": "2022",
         "descripcion": "Plataforma de audio para musicos independientes al estilo youtube.",
-        "lenguaje": "Java - Spring Boot - Docker - AWS - Js - HTML - Css",
+        "lenguaje": "Java - SpringBoot - Docker - AWS - Js - HTML - Css",
         "repo": "https://github.com/dsrrdiego/emergeSpringBoot.git",
         "link": "https://dsrrdiego.github.io/emergeF/",
         "linkTexto": "Visitar"
--- js/main.js
+++ js/main.js
@@ -1,5 +1,9 @@
 //boton buscar
 ///sector search Render
+// let carrusel = document.getElementById("sectorCarruseles");
+let slide = document.getElementById('slide');
+document.getElementById('btnSlideMenos').addEventListener('click', () => btnSlide(-1));
+document.getElementById('btnSlideMas').addEventListener('click', () => btnSlide(1));
 
 let filtro = [];
 let sectorTags = document.getElementById('sectionTags');
@@ -62,32 +66,84 @@ function filtrar(palabra = '') {
         if (agregarXPalabra && agregarXTag) card.push(c);
 
     }
-    render()
+    render2()
 }
 
 
 
 ///Sector cards Render
-let section = document.getElementById('section');
+// let section = document.getElementById('section');
 
-function render() {
-    section.innerHTML = "";
-    card.forEach(card => {
-        let string = '<article>';
-        // if (card.video != "") string += '<div class="video"><iframe class="video" src="' + card.video + '" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> </div>';
-        // else {
-        //     string += '<img class="video" src="imagenes/' + card.imagen + '"></img>';
-        // }
-        string += '<div class="titulo"><h2>' + card.titulo + '</h2></div>';
-        string += '<div class="datos"> <div class="categoria"> <h3>' + card.categoria + '</h3> </div>'
-        string += '<div class="categoria fecha"><h5>' + card.fecha + '</h5></div>';
-        string += '<div class="descripcion"><h4>' + card.descripcion + '</h4> </div> <br>';
-        string += '<div class="lenguaje"> <h4>Lenguaje: ' + card.lenguaje + '</h4></div>';
-        if (card.repo != "") string += '<div class="repo"><a target="_blank" class="repoLink" href="' + card.repo + '">Ir al Repositorio GitHub</a></h4></div>';
-        if (card.link != "") string += '<button class="boton"> <a target="_blank" href="' + card.link + '">' + card.linkTexto + '</a> </button></div></article>';
-        string += '</article>';
-        section.innerHTML += string;
-    })
-}
+// function render() {
+//     section.innerHTML = "";
+//     card.forEach(card => {
+//         let string = '<article>';
+//         // if (card.video != "") string += '<div class="video"><iframe class="video" src="' + card.video + '" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> </div>';
+//         // else {
+//         //     string += '<img class="video" src="imagenes/' + card.imagen + '"></img>';
+//         // }
+//         string += '<div class="titulo"><h2>' + card.titulo + '</h2></div>';
+//         string += '<div class="datos"> <div class="categoria"> <h3>' + card.categoria + '</h3> </div>'
+//         string += '<div class="categoria fecha"><h5>' + card.fecha + '</h5></div>';
+//         string += '<div class="descripcion"><h4>' + card.descripcion + '</h4> </div> <br>';
+//         string += '<div class="lenguaje"> <h4>Lenguaje: ' + card.lenguaje + '</h4></div>';
+//         if (card.repo != "") string += '<div class="repo"><a target="_blank" class="repoLink" href="' + card.repo + '">Ir al Repositorio GitHub</a></h4></div>';
+//         if (card.link != "") string += '<button class="boton"> <a target="_blank" href="' + card.link + '">' + card.linkTexto + '</a> </button></div></article>';
+//         string += '</article>';
+//         section.innerHTML += string;
+//     })
+// }
 
 filtrar();
+////let indice = [];
+
+function render2() {
+    slide.innerHTML = "";
+
+    for (let i = 0; i < card.length; i++) {
+        let c = crearCard(card[i]);
+        slide.innerHTML += c;
+    }
+
+}
+
+function btnSlide(sentido) {
+    // indice[i] += sentido;
+    // if (indice[i] == -1) indice[i] = carruseles[i].cards.length - 1;
+    // if (indice[i] == carruseles[i].cards.length) indice[i] = 0;
+    // console.log(indice[i]);
+    // const sli = document.getElementById("slide" + i);
+    slide.scrollLeft += 100 * sentido;
+    // slide.scrollLeft = indice[i] * sli.offsetWidth;
+    // }
+
+    //     // document.getElementById("enviar").addEventListener("click", enviar);
+    //     // document.getElementById("mensaje");
+
+    //     // function enviar() {
+    //     //     var link =
+    //     //         "https://wa.me/" +
+    //     //         "+5492983606824" +
+    //     //         "?text=" +
+    //     //         encodeURIComponent(mensaje.value);
+    //     //     window.open(link);
+}
+
+
+
+function crearCard(card) {
+    let string = '<article>creada';
+    // if (card.video != "") string += '<div class="video"><iframe class="video" src="' + card.video + '" title="" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe> </div>';
+    // else {
+    //     string += '<img class="video" src="imagenes/' + card.imagen + '"></img>';
+    // }
+    string += '<div class="titulo"><h2>' + card.titulo + '</h2></div>';
+    string += '<div class="datos"> <div class="categoria"> <h3>' + card.categoria + '</h3> </div>'
+    string += '<div class="categoria fecha"><h5>' + card.fecha + '</h5></div>';
+    string += '<div class="descripcion"><h4>' + card.descripcion + '</h4> </div> <br>';
+    string += '<div class="lenguaje"> <h4>Lenguaje: ' + card.lenguaje + '</h4></div>';
+    if (card.repo != "") string += '<div class="repo"><a target="_blank" class="repoLink" href="' + card.repo + '">Ir al Repositorio GitHub</a></h4></div>';
+    if (card.link != "") string += '<button class="boton"> <a target="_blank" href="' + card.link + '">' + card.linkTexto + '</a> </button></div></article>';
+    string += '</article>';
+    return string;
+}
