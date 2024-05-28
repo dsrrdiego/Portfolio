f02d2b7f1ae01a81d2d95d55d100a3850757475d
Diego Sebastian Rodriguez Recalde <dsrrdiego@hotmail.com>
2024-05-28 13:51:38 -0300

    nchor cambiado de lugar

--- js/tags.js
+++ js/tags.js
@@ -8,26 +8,3 @@ for (const t of tagss) {
     tags.push(tag);
 
 }
-
-// let tagsX = [{
-//     "nombre": 'Angular',
-//     "activo": false
-// }, {
-//     "nombre": 'C++',
-//     "activo": false
-// }, {
-//     "nombre": 'Java ',
-//     "activo": false
-// }, {
-//     "nombre": 'JavaScript',
-//     "activo": false
-// }, {
-//     "nombre": 'Juego',
-//     "activo": false
-// }, {
-//     "nombre": 'OpenGL',
-//     "activo": false
-// }, {
-//     "nombre": 'astronómica',
-//     "activo": false
-// }];
